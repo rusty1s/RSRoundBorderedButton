047b64decff94d2de397f2c175b771dee3666e16
Matthias Fey <matthias.fey@tu-dortmund.de>
2015-05-20 20:22:33 +0200

    Initial Cocoa Touch Framework Commit

---
+++ RSRoundBorderedButton/RSRoundBorderedButton.h
@@ -0,0 +1,19 @@
+//
+//  RSRoundBorderedButton.h
+//  RSRoundBorderedButton
+//
+//  Created by Matthias Fey on 20.05.15.
+//  Copyright (c) 2015 Matthias Fey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSRoundBorderedButton.
+FOUNDATION_EXPORT double RSRoundBorderedButtonVersionNumber;
+
+//! Project version string for RSRoundBorderedButton.
+FOUNDATION_EXPORT const unsigned char RSRoundBorderedButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSRoundBorderedButton/PublicHeader.h>
+
+
