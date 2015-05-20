414eeb21b2867f277d9fc15f63bf868a1830c121
Matthias Fey <matthias.fey@tu-dortmund.de>
2015-05-20 20:29:50 +0200

    Credits in Code

--- RSRoundBorderedButton/RSRoundBorderedButton.swift
+++ RSRoundBorderedButton/RSRoundBorderedButton.swift
@@ -1,5 +1,6 @@
 //
 //  RSRoundBorderedButton.swift
+//  Credits: https://github.com/yhpark/YHRoundBorderedButton
 //
 //  Created by Matthias Fey on 03.02.15.
 //  Copyright (c) 2015 Rusty1s. All rights reserved.
@@ -11,9 +12,8 @@ class RSRoundBorderedButton : UIButton {
     
     // MARK: Initializers
     
-    init() {
-        super.init(frame: CGRectZero)
-        setup()
+    convenience init() {
+        self.init(frame: CGRectZero)
     }
     
     override init(frame: CGRect) {
@@ -22,7 +22,8 @@ class RSRoundBorderedButton : UIButton {
     }
 
     required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        setup()
     }
     
     // MARK: Setup
